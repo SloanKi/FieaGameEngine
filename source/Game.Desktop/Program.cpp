bac1bcb64f25362e7e92edc7366b29b78f47a037
Sloan Kiechel <sloan.kiechel@gmail.com>
2020-05-05 17:31:53 -0400

    Add files via upload

---
+++ source/Game.Desktop/Program.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "SList.h"
+
+
+//SList<int> CreateThings();
+int main()
+{
+	/*SList<int> list;
+	list.PushBack(10);
+
+	SList<int> anotherList = std::move(list);
+	SList<int> anotherList2(std::move(list));
+
+	SList<int> b = CreateThings();		//move constructor
+	SList<int> a(CreateThings()); */	//move constructor
+
+	//Library::foo f;
+	//f.Data();
+	return 0;
+}
+
+//SList<int> CreateThings()
+//{
+//	SList<int> list;
+//	return list;
+//}
